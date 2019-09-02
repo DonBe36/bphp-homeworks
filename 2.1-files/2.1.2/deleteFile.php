08ce1474e17fa0b503c7025d3bb338e2f299112b
Vasya Pupkin <vasya@localhost>
2019-09-02 21:43:40 +0300

    4 com

---
+++ 2.1-files/2.1.2/deleteFile.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['delete'])) {
+    deleteFile($_POST['fileToDelete'], $uploadsDir);
+};
+function deleteFile($fileName, $dir)
+{
+    if (file_exists($dir.$fileName)) {
+        unlink($dir.$fileName);
+        echo 'File successfully deleted';
+    } else {
+        echo 'File Not Exist';
+    }
+};
---
+++ 2.1-files/2.1.2/uploadToDir.php
@@ -0,0 +1,29 @@
+<?php
+$uploadsDir = __DIR__ . '/uploads/';
+$fileName = 'picture';
+if(isset($_POST['upload'])) {
+    uploadFileToDir($uploadsDir, $fileName);
+};
+function uploadFileToDir($uploadsDir='', $fileName='') 
+{
+    $extType = [
+        'gif', 
+        'jpg', 
+        'jpe', 
+        'jpeg', 
+        'png',
+    ];
+    if ($_FILES[$fileName]['error'] === 0) {
+        $tmpName = $_FILES[$fileName]['tmp_name'];
+        
+        if (is_uploaded_file($tmpName)) {
+            $pathParts = pathinfo($_FILES[$fileName]['name']);
+            if (in_array($pathParts['extension'], $extType)) {
+                return move_uploaded_file(
+                    $tmpName,
+                    $uploadsDir.$pathParts['basename']
+                );
+            };
+        };
+    };
+};
